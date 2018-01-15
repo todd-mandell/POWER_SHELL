c3cc41e07febd501fc823d6f081791c4f0fdd82c
Todd Mandell <todd.mandell@outlook.com>
2018-01-14 22:08:47 -0500

    Create AD_NewUser.ps1

---
+++ AD_NewUser.ps1
@@ -0,0 +1,6 @@
+$NewUserName = "User1000"
+$PlainPassword = get-date -format "MMMMMMMMMMMMFF!"
+$SecurePassword = $PlainPassword | ConvertTo-SecureString -AsPlainText -Force 
+$Display = write "UN " $NewUserName "PW " $PlainPassword;write $Display
+write $Display | out-file C:\AD-logs\NewADUsersLog.log -Append
+new-aduser $NewUserName -AccountPassword $SecurePassword -ChangePasswordAtLogon 1 -Enabled 1
