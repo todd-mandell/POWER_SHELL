1a4863517a7b9cd0a673270fba8e04cb242f851d
Todd Mandell <todd.mandell@outlook.com>
2018-02-03 17:21:41 -0500

    Update Uninstall_Chrome_Browser.ps1

--- Uninstall_Chrome_Browser.ps1
+++ Uninstall_Chrome_Browser.ps1
@@ -1,4 +1,3 @@
-Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like "*Google*"} | foreach-object -process {$_.Uninstall()}
-##needs another line to delete the operational folder
-#Shortened
-gwmi Win32_Product | Where {$_.Name -like "*Google*"} | foreach -process {$_.Uninstall()}
+spps -name *google*,*chrome* -force
+gwmi -Class Win32_Product | Where-Object {$_.Name -like "*Google*"} | foreach-object -process {$_.Uninstall()}
+ri 'C:\Program Files (x86)\Google' -recurse
