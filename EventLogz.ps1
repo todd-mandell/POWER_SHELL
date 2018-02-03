a79bcf2b512fbe4a8a250309f41af40d08e82837
Todd Mandell <todd.mandell@outlook.com>
2018-02-03 17:41:19 -0500

    Update EventLogz.ps1

--- EventLogz.ps1
+++ EventLogz.ps1
@@ -1,5 +1,12 @@
-###lots of changes to this one coming soon! I made it so it time stamps the output filenames too for evidence
-
-get-eventlog system -message "*power*" | out-file -width 200 c:\powerEvents.txt
-
-Get-EventLog system -entrytype Error -Newest 20 | select Message
+$Dait = get-date -format 'MM-dd-yyyy'
+$Locale = 'c:\'
+$RecoverString = 'get-eventlog -log "System" -message "*recover*" | select TimeGenerated,Message | out-file -width 200 ' + $Locale + 'recoveryEvents' + $Dait + '.txt'
+$PowerString = 'get-eventlog -log "System" -message "*shutdown*" | select TimeGenerated,Message | out-file -width 200 ' + $Locale + 'powerEvents' + $Dait + '.txt'
+$DNSString = 'get-eventlog -log "System" -message "*DNS*" | select TimeGenerated,Message | out-file -width 200 ' + $Locale + 'DNS-Events' + $Dait + '.txt'
+$DHCPString = 'get-eventlog -log "System" -message "*dhcp*" | select TimeGenerated,Message | out-file -width 200 ' + $Locale + 'DHCP-Events' + $Dait + '.txt'
+$DiskString = 'get-eventlog -log "System" -message "*disk*" | select TimeGenerated,Message | out-file -width 200 ' + $Locale + 'DISK-Events' + $Dait + '.txt'
+iex $RecoverString
+iex $PowerString
+iex $DNSString
+iex $DHCPString
+iex $DiskString
