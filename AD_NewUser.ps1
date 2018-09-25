3ff4e22aa94617a0b8fa268108396ab485daf71f
Todd Mandell <todd.mandell@outlook.com>
2018-09-25 18:09:50 -0400

    Update AD_NewUser.ps1

--- AD_NewUser.ps1
+++ AD_NewUser.ps1
@@ -1,6 +1,5 @@
-$NewUserName = "User1000"
-$PlainPassword = get-date -format "MMMMFFF!"
-$SecurePassword = $PlainPassword | ConvertTo-SecureString -AsPlainText -Force 
-$Display = write "UN " $NewUserName "PW " $PlainPassword;write $Display
-write $Display | out-file C:\AD-logs\NewADUsersLog.log -Append
-new-aduser $NewUserName -AccountPassword $SecurePassword -ChangePasswordAtLogon 1 -Enabled 1
+$UN = "Username";$FN = "FirstName";$LN = "LastName";
+$FULLN = $FN + " " + $LN;$PlainPassword = get-date -format "MMMMFFF!";
+$SecPW = $PlainPassword | ConvertTo-SecureString -AsPlainText -Force;
+$Display = write "un " $UN "pw " $PlainPassword;write $Display;write $Display | out-file C:\NewADUsersLog.txt -Append;
+new-aduser -name $FULLN -UserPrincipalName $UN -displayname $FULLN -samaccountname $UN -givenname $FN -surname $LN -AccountPassword $SecPW -ChangePasswordAtLogon 1 -Enabled 1
