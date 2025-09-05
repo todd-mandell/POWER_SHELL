630712f3d8b0cad334e10fd50c4b9b6598d23021
Todd Mandell <todd.mandell@outlook.com>
2025-09-05 12:03:44 -0400

    Create PS-EthernetLinkStater.ps1

---
+++ PS-EthernetLinkStater.ps1
@@ -0,0 +1,8 @@
+$adapter = "WindowsAdapterName"
+$x=1
+while($x=1) {
+$stat1 = Get-NetAdapter | where {$_.Name -like $adapter} | select Status
+$outputz = $adapter + $stat1
+if($stat1 -like "*Up*"){write-host $outputz -ForegroundColor Green } else {write-host $outputz -ForegroundColor Red}
+            }
+					
